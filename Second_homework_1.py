7a798b77c147b1997b9cedb6a03b2b335901a35e
smohorzhevskyi <75439124+smohorzhevskyi@users.noreply.github.com>
2020-12-05 02:04:04 +0200

    Add files via upload

--- Second_homework_1.py
+++ Second_homework_1.py
@@ -2,22 +2,5 @@
 Напишите lambda функцию, которая принимает два аргумента x и y и возвращает результат деления x на y
 при условии, что делитель не равен 0, иначе возвращаем None.
 """
-x, y = [int(i) for i in input().split()]
-result = lambda x, y: x / y
 
-try:
-    print(result(x, y))
-except (ZeroDivisionError, ValueError):
-    print("None")
-
-
-"Это не совсем по PEP8 - потому я переписал ниже"
-
-# x, y = [int(i) for i in input().split()]
-# def division(*args): return x / y
-#
-#
-# try:
-#     print(division(x, y))
-# except (ZeroDivisionError, ValueError):
-#     print("None")
+(lambda x, y: print(x / y) if y != 0 else None)(x=float(input()), y=float(input()))
