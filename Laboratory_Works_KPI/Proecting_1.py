86008badf3d6e1c1f558321f0fad5639b1423fa2
smohorzhevskyi <75439124+smohorzhevskyi@users.noreply.github.com>
2021-02-11 12:01:21 +0200

    Add files via upload

---
+++ Laboratory_Works_KPI/Proecting_1.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+S = [1, 3, 5, 6, 7, 8, 9, 10]
+A = 0
+K = int(input("Input what K-biggest element to find in S: "))
+
+while K < 1 or K > len(S):
+    K = int(input(f"K shouldn't be bigger than {len(S)} or less than 1: "))
+
+before = datetime.now()
+
+
+def k_biggest(array, k):
+    array.sort()
+    global A
+    A = array[-k]
+    S.remove(A)
+    S.insert(k, A)
+    return S
+
+
+after = datetime.now()
+print(f"{(after - before).total_seconds() * 1000} is code exec time in ms")
+print(f"{K}-biggest in {k_biggest(S, K)} is {S[K-1]} at position of {S.index(A)} (count from 0)")
+
